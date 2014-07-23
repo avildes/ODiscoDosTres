3fc41f2180a493726218704c8f5a61e8651199b5
Antonio Vildes Barbosa <antoniovildes@joystreet.com.br>
2014-07-22 22:24:39 -0300

    puppet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10f;
+    public Vector2
+         maxVelocity = new Vector2(3, 5);
 
-	void Start ()
+    public bool standing;
+    public float jumpSpeed;
+
+    private Vector2 rigidbodyForce;
+
+    private Animator playerAnimator;
+    private PlayerDefaultController pDefController;
+
+    void Start()
     {
-	
-	}
-	
-	
+        playerAnimator = gameObject.GetComponent<Animator>();
+        pDefController = gameObject.GetComponent<PlayerDefaultController>();
+    }
+
 	void Update ()
     {
         ProcessInputs();
@@ -17,6 +28,45 @@ public class PlayerController : MonoBehaviour
 
     void ProcessInputs()
     {
+        float forceX = 0f;
+        float forceY = 0f;
 
+        float absVelX = Mathf.Abs(rigidbody2D.velocity.x);
+        float absVelY = Mathf.Abs(rigidbody2D.velocity.y);
+
+        if(absVelY < .2f)
+        {
+            standing = true;
+        }
+        else
+        {
+            standing = false;
+        }
+
+        if(pDefController.moving.x !=0)
+        {
+            if (absVelX < maxVelocity.x)
+                forceX = speed * pDefController.moving.x;
+
+            transform.localScale = new Vector3(pDefController.moving.x, 1, 1);
+
+            playerAnimator.SetInteger("Walk", 1);   
+        }
+        else
+        {
+            playerAnimator.SetInteger("Walk", 0);
+        }
+
+        if(pDefController.moving.y > 0 && standing)
+        {
+            if (absVelY < maxVelocity.y)
+                forceY = jumpSpeed;
+        }
+
+        rigidbodyForce = new Vector2(forceX, forceY);
+    }
+    void FixedUpdate()
+    {
+        rigidbody2D.AddForce(rigidbodyForce);
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    private Animator playerAnimator;
+
+    public float jumpForce = 700f;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        
+        playerAnimator.SetBool("Ground", grounded);
+
+        playerAnimator.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+        Movement();
+	}
+
+    void Update()
+    {
+        //TODO criar um botao generico para pular
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnimator.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        playerAnimator.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
